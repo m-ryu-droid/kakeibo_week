bdc61b156e6a09a72210835c364efefaa54265fa
m-ryu-droid <m-ryu@co-success.jp>
2026-07-27 18:58:51 +0900

    Fix current week and add expense presets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "weekly-kakeibo-v5";
+const CACHE_NAME = "weekly-kakeibo-v6";
 const APP_FILES = ["./", "./index.html", "./kakeibo.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
